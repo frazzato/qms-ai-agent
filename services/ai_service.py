0a2906b14d6350f5d85ad2308bbd6154a1250349
Marcelo Frazzato <107005775+frazzato@users.noreply.github.com>
2026-05-08 07:53:27 -0500

    Update ai_service.py

--- services/ai_service.py
+++ services/ai_service.py
@@ -1,5 +1,10 @@
+import google.generativeai as genai
+from config.settings import API_KEY, GEMINI_MODEL
+from config.safety import SAFETY_SETTINGS
+
+genai.configure(api_key=API_KEY)
+
 model = genai.GenerativeModel(
     model_name=GEMINI_MODEL,
     safety_settings=SAFETY_SETTINGS
 )
-
